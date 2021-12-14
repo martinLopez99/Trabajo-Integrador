c90b16e53b363b7b4b60d0baf90393d2937c781d
Renato Corbellini Pezzatti <renacorbe@gmail.com>
2021-12-14 16:50:57 -0300

    Agregue la Interfaz de Usuario de la pantalla principal

---
+++ Prestamos Biblioteca/GUIPrestamosBiblioteca/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace GUIPrestamosBiblioteca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tituloPrincipal = new System.Windows.Forms.Label();
+            this.iniciarSesionAdmBoton = new System.Windows.Forms.Button();
+            this.salirBoton = new System.Windows.Forms.Button();
+            this.nombreUsuarioTextbox = new System.Windows.Forms.TextBox();
+            this.contrasenia = new System.Windows.Forms.Label();
+            this.nombreUsuario = new System.Windows.Forms.Label();
+            this.contraseniaTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tituloPrincipal
+            // 
+            this.tituloPrincipal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tituloPrincipal.AutoSize = true;
+            this.tituloPrincipal.Font = new System.Drawing.Font("Bahnschrift Condensed", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tituloPrincipal.Location = new System.Drawing.Point(81, 9);
+            this.tituloPrincipal.Name = "tituloPrincipal";
+            this.tituloPrincipal.Size = new System.Drawing.Size(497, 35);
+            this.tituloPrincipal.TabIndex = 0;
+            this.tituloPrincipal.Text = "Bienvenido a la App de Prestamos de la Biblioteca";
+            // 
+            // iniciarSesionAdmBoton
+            // 
+            this.iniciarSesionAdmBoton.Font = new System.Drawing.Font("Bahnschrift Light", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.iniciarSesionAdmBoton.Location = new System.Drawing.Point(272, 202);
+            this.iniciarSesionAdmBoton.Name = "iniciarSesionAdmBoton";
+            this.iniciarSesionAdmBoton.Size = new System.Drawing.Size(130, 43);
+            this.iniciarSesionAdmBoton.TabIndex = 1;
+            this.iniciarSesionAdmBoton.Text = "Iniciar Sesión";
+            this.iniciarSesionAdmBoton.UseVisualStyleBackColor = true;
+            // 
+            // salirBoton
+            // 
+            this.salirBoton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.salirBoton.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.salirBoton.Location = new System.Drawing.Point(584, 271);
+            this.salirBoton.Name = "salirBoton";
+            this.salirBoton.Size = new System.Drawing.Size(88, 33);
+            this.salirBoton.TabIndex = 2;
+            this.salirBoton.Text = "Salir";
+            this.salirBoton.UseVisualStyleBackColor = true;
+            // 
+            // nombreUsuarioTextbox
+            // 
+            this.nombreUsuarioTextbox.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nombreUsuarioTextbox.Location = new System.Drawing.Point(305, 86);
+            this.nombreUsuarioTextbox.Name = "nombreUsuarioTextbox";
+            this.nombreUsuarioTextbox.Size = new System.Drawing.Size(129, 27);
+            this.nombreUsuarioTextbox.TabIndex = 5;
+            // 
+            // contrasenia
+            // 
+            this.contrasenia.AutoSize = true;
+            this.contrasenia.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contrasenia.Location = new System.Drawing.Point(202, 137);
+            this.contrasenia.Name = "contrasenia";
+            this.contrasenia.Size = new System.Drawing.Size(97, 19);
+            this.contrasenia.TabIndex = 4;
+            this.contrasenia.Text = "Contraseña:";
+            this.contrasenia.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nombreUsuario
+            // 
+            this.nombreUsuario.AutoSize = true;
+            this.nombreUsuario.Font = new System.Drawing.Font("Bahnschrift SemiLight", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.nombreUsuario.Location = new System.Drawing.Point(145, 89);
+            this.nombreUsuario.Name = "nombreUsuario";
+            this.nombreUsuario.Size = new System.Drawing.Size(154, 19);
+            this.nombreUsuario.TabIndex = 3;
+            this.nombreUsuario.Text = "Nombre de Usuario:";
+            this.nombreUsuario.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contraseniaTextbox
+            // 
+            this.contraseniaTextbox.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contraseniaTextbox.Location = new System.Drawing.Point(305, 137);
+            this.contraseniaTextbox.Name = "contraseniaTextbox";
+            this.contraseniaTextbox.Size = new System.Drawing.Size(129, 27);
+            this.contraseniaTextbox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 316);
+            this.Controls.Add(this.contraseniaTextbox);
+            this.Controls.Add(this.nombreUsuarioTextbox);
+            this.Controls.Add(this.contrasenia);
+            this.Controls.Add(this.nombreUsuario);
+            this.Controls.Add(this.salirBoton);
+            this.Controls.Add(this.iniciarSesionAdmBoton);
+            this.Controls.Add(this.tituloPrincipal);
+            this.MinimumSize = new System.Drawing.Size(700, 280);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tituloPrincipal;
+        private System.Windows.Forms.Button iniciarSesionAdmBoton;
+        private System.Windows.Forms.Button salirBoton;
+        private System.Windows.Forms.TextBox nombreUsuarioTextbox;
+        private System.Windows.Forms.Label contrasenia;
+        private System.Windows.Forms.Label nombreUsuario;
+        private System.Windows.Forms.TextBox contraseniaTextbox;
+    }
+}
+
